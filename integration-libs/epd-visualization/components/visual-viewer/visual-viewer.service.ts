7cea9fe26a6499e4155d4fba8f65a476509de0c6
Sam Garland <sam.garland@sap.com>
2021-12-16 23:16:05 +1300

    Revert "chore: removed pointless method"
    This reverts commit 7a793aa18c04164fb170989398b96194e8505c96.

--- integration-libs/epd-visualization/components/visual-viewer/visual-viewer.service.ts
+++ integration-libs/epd-visualization/components/visual-viewer/visual-viewer.service.ts
@@ -977,6 +977,10 @@ export class VisualViewerService implements OnDestroy {
     return !!window.sap;
   }
 
+  private getCore(): Core {
+    return sap.ui.getCore();
+  }
+
   private bootstrapUi5(scriptElementId: string): Observable<void> {
     const epdVisualization = this.epdVisualizationConfig
       .epdVisualization as EpdVisualizationInnerConfig;
@@ -1016,7 +1020,7 @@ export class VisualViewerService implements OnDestroy {
   private initializeUi5(): Observable<void> {
     return new Observable(
       (subscriber: { next: () => void; complete: () => void }) => {
-        const core: Core = sap.ui.getCore();
+        const core: Core = this.getCore();
         core.attachInit(() => {
           const loadLibraryOptions = { async: true };
           Promise.all([
@@ -1034,7 +1038,7 @@ export class VisualViewerService implements OnDestroy {
   }
 
   private destroyViewportAssociations(viewport: Viewport): void {
-    const core = sap.ui.getCore();
+    const core = this.getCore();
     if (!core) {
       return;
     }
@@ -1190,7 +1194,7 @@ export class VisualViewerService implements OnDestroy {
             sap_ui_vk_ContentConnector: any,
             sap_ui_vk_DrawerToolbar: any
           ) => {
-            const core: Core = sap.ui.getCore();
+            const core: Core = this.getCore();
             const uiArea: UIArea = core.getUIArea(
               this.elementRef.nativeElement
             );
